8f04bc7c5a72389b12d317aec09667842f960329
lisanchez <liz.sanchez@ellucian.com>
2016-04-20 12:48:12 -0600

    group folder view test and domain

---
+++ test/integration/net/hedtech/banner/csr/GroupFolderReadOnlyIntegrationTests.groovy
@@ -0,0 +1,43 @@
+/*********************************************************************************
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
+ **********************************************************************************/
+
+package net.hedtech.banner.csr
+
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class GroupFolderReadOnlyIntegrationTests extends BaseIntegrationTestCase {
+
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+    }
+
+
+    @After
+    public void tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    void testFetchGroupFolder( ) {
+        List<GroupFolderReadOnly> groupFolderList = GroupFolderReadOnly.fetchGroupFolders()
+        assertFalse groupFolderList.isEmpty(  )
+        println groupFolderList
+    }
+
+    @Test
+    void testFetchGroupFolderById() {
+        List<GroupFolderReadOnly> groupFolderList = GroupFolderReadOnly.fetchGroupFolders()
+        def groupFolderId = groupFolderList[0].groupId
+        List<GroupFolderReadOnly> groupFolderListById = GroupFolderReadOnly.fetchGroupFoldersById( groupFolderId )
+        assert 1 <= groupFolderListById.size()
+        println groupFolderListById
+    }
+
+}
