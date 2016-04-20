7e27864ee5f697de7b10f52dda01eca27f5736a9
lisanchez <liz.sanchez@ellucian.com>
2016-04-20 15:38:44 -0600

    BXCSR-214 naming changes to action item group view

--- src/groovy/net/hedtech/banner/csr/GroupFolderReadOnly.groovy
+++ src/groovy/net/hedtech/banner/csr/GroupFolderReadOnly.groovy
@@ -19,12 +19,12 @@ import javax.persistence.*
 ])
 
 @Entity
-@Table(name = "GVQ_GCRGFLDR")
+@Table(name = "GVQ_GCBAGRP")
 
 class GroupFolderReadOnly implements Serializable {
 
     /**
-     *  ID for GVQ_GCRGFLDR
+     *  ID for the action item group
      */
 
     @Id
@@ -32,7 +32,7 @@ class GroupFolderReadOnly implements Serializable {
     Long groupId
 
     /**
-     * Title of the action item group
+     * action item group title
      */
 
     @Column(name = "ACTION_ITEM_GROUP_TITLE")
@@ -46,7 +46,7 @@ class GroupFolderReadOnly implements Serializable {
     String groupDesc
 
     /**
-     * Last activity date for the action item
+     * status of the action item group
      */
 
     @Column(name = "ACTION_ITEM_GROUP_STATUS")
@@ -54,41 +54,42 @@ class GroupFolderReadOnly implements Serializable {
 
     /*
     **
-    * User action item pertains to
+    * date the action item group was last modified
     */
     @Column(name = "ACTION_ITEM_GROUP_ADATE")
     Date groupActivityDate
 
     /**
-     * Description for action item
+     * userid last modified group
      */
     @Column(name = "ACTION_ITEM_GROUP_USERID")
     /*need to figure out what to limit length to for display*/
     String groupUserId
 
     /**
-     * UserID that created the action item
+     * Version of the action item group
      */
     @Version
     @Column(name = "ACTION_ITEM_GROUP_VERSION")
     Long groupVersion
 
-    /**
-     * Date the action item was created
-     */
-    @Column(name = "ACTION_ITEM_FOLDER_ID")
-    Long folderId
 
     /**
-     * Version of the action item
+     * VPDI Code
+     */
+
+    @Column(name = "ACTION_ITEM_GROUP_VPDI_CODE")
+    String groupVpdiCode
+
+    /**
+     * Folder name
      */
 
     @Column(name = "ACTION_ITEM_FOLDER_NAME")
     String folderName
 
-
     /**
-     * PIDM of the user action item belongs to
+     * Folder Description
      */
     @Column(name = "ACTION_ITEM_FOLDER_DESC")
     String folderDesc
@@ -103,7 +104,7 @@ class GroupFolderReadOnly implements Serializable {
                 ", groupActivityDate=" + groupActivityDate +
                 ", groupUserId='" + groupUserId + '\'' +
                 ", groupVersion=" + groupVersion +
-                ", folderId=" + folderId +
+                ", groupVpdiCode='" + groupVpdiCode + '\'' +
                 ", folderName='" + folderName + '\'' +
                 ", folderDesc='" + folderDesc + '\'' +
                 '}';
@@ -116,15 +117,15 @@ class GroupFolderReadOnly implements Serializable {
         GroupFolderReadOnly that = (GroupFolderReadOnly) o
 
         if (folderDesc != that.folderDesc) return false
-        if (folderId != that.folderId) return false
         if (folderName != that.folderName) return false
         if (groupActivityDate != that.groupActivityDate) return false
         if (groupDesc != that.groupDesc) return false
         if (groupId != that.groupId) return false
-        if (groupVersion != that.groupVersion) return false
         if (groupStatus != that.groupStatus) return false
         if (groupTitle != that.groupTitle) return false
         if (groupUserId != that.groupUserId) return false
+        if (groupVersion != that.groupVersion) return false
+        if (groupVpdiCode != that.groupVpdiCode) return false
 
         return true
     }
@@ -138,12 +139,11 @@ class GroupFolderReadOnly implements Serializable {
         result = 31 * result + (groupActivityDate != null ? groupActivityDate.hashCode() : 0)
         result = 31 * result + (groupUserId != null ? groupUserId.hashCode() : 0)
         result = 31 * result + (groupVersion != null ? groupVersion.hashCode() : 0)
-        result = 31 * result + (folderId != null ? folderId.hashCode() : 0)
+        result = 31 * result + (groupVpdiCode != null ? groupVpdiCode.hashCode() : 0)
         result = 31 * result + (folderName != null ? folderName.hashCode() : 0)
         result = 31 * result + (folderDesc != null ? folderDesc.hashCode() : 0)
         return result
     }
-
 
     public static def fetchGroupFolders( ) {
         GroupFolderReadOnly.withSession { session ->
@@ -158,5 +158,4 @@ class GroupFolderReadOnly implements Serializable {
             return groupFolderListById
         }
     }
-
 }
