9be38ddcdb64594204d96da3f5a816dd2bd6748b
Sanchez, Liz <Liz.Sanchez@ellucian.com>
2016-12-16 16:05:32 -0700

    returning process properties in service

--- grails-app/services/net/hedtech/banner/aip/ActionItemBlockedProcessService.groovy
+++ grails-app/services/net/hedtech/banner/aip/ActionItemBlockedProcessService.groovy
@@ -4,7 +4,6 @@
 
 package net.hedtech.banner.aip
 
-
 import grails.converters.JSON
 import org.apache.log4j.Logger
 import net.hedtech.banner.configuration.ConfigurationData
@@ -12,12 +11,29 @@ import net.hedtech.banner.configuration.ConfigurationData
 
 class ActionItemBlockedProcessService {
 
+    def static final types = [name:'string', value:'json']
+
     def listBlockedProcesses() {
         return ConfigurationData.fetchConfigurationData()
     }
 
     def listBlockedProcessesByName( String myConfigName ) {
         return ConfigurationData.fetchConfigurationDataByName( myConfigName )
+    }
+
+    def listBlockedProcessProps(String myConfigName) {
+
+        def configData = ConfigurationData.fetchConfigurationDataByName( myConfigName )
+        def configDataJson = JSON.parse( configData.value.toString() ) as ConfigObject
+        def configProps = configDataJson.toProperties()
+
+        String urlProp = "aipBlockedProcess." + myConfigName + ".urls"
+        String i18nProp = "aipBlockedProcess." + myConfigName + ".i18n"
+
+        def processMap = [ url: configProps[urlProp], i18n:configProps[i18nProp] ]
+
+        return processMap
+
     }
 
 }
--- test/integration/net/hedtech/banner/aip/ActionItemBlockedProcessServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/aip/ActionItemBlockedProcessServiceIntegrationTests.groovy
@@ -9,6 +9,8 @@ import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import grails.converters.JSON
+
 
 
 class ActionItemBlockedProcessServiceIntegrationTests extends BaseIntegrationTestCase {
@@ -41,12 +43,26 @@ class ActionItemBlockedProcessServiceIntegrationTests extends BaseIntegrationTes
     void testListBlockedProcessByName() {
 
         List<ConfigurationData> configData = actionItemBlockedProcessService.listBlockedProcesses(  )
-
         List<ConfigurationData> configDataByName = actionItemBlockedProcessService.listBlockedProcessesByName( configData.name[0] )
 
         assertEquals( configData.name[0], configDataByName.name[0] )
         assertEquals( configData.value[0], configDataByName.value[0] )
         assertEquals( configData.type[0], configDataByName.type[0] )
+
+    }
+
+
+    @Test
+    void testParseBlockedProcessJSON() {
+
+        List<ConfigurationData> configData = actionItemBlockedProcessService.listBlockedProcesses(  )
+        def processProps = actionItemBlockedProcessService.listBlockedProcessProps( configData.name[0].toString() )
+
+       // def blockedProcessJSON = configDataByName.value[0]
+
+        //assertEquals( configData.value[0], blockedProcessJSON )
+
+        println processProps
 
     }
 
