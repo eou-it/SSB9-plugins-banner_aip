080b3649e410e5d9481384eb20f5a28010961e28
Tiwari Shiv <shiv.tiwari@ellucian.com>
2017-10-20 22:28:30 +0530

    API to save posting job

--- grails-app/services/net/hedtech/banner/aip/common/ActionItemProcessingCommonService.groovy
+++ grails-app/services/net/hedtech/banner/aip/common/ActionItemProcessingCommonService.groovy
@@ -4,10 +4,17 @@
 
 package net.hedtech.banner.aip.common
 
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.exceptions.BusinessLogicValidationException
 import net.hedtech.banner.general.communication.folder.CommunicationFolder
+import net.hedtech.banner.i18n.LocalizeUtil
 import net.hedtech.banner.general.communication.population.CommunicationPopulationListView
+import org.apache.log4j.Logger
 
 class ActionItemProcessingCommonService {
+    def dateConverterService
+
+    private static final def LOGGER = Logger.getLogger( this.getClass() )
 
     /**
      * Gets Communication folders
@@ -16,12 +23,29 @@ class ActionItemProcessingCommonService {
     def fetchCommunicationFolders() {
         CommunicationFolder.list( sort: "name", order: "asc" )
     }
-
     /**
      * Gets Population List For Send
      * @return
      */
     def fetchPopulationListForSend( searchParam, paginationParam ) {
+        println 'search Param' + searchParam
         CommunicationPopulationListView.findAllForSendByPagination( [params: [name: searchParam]], paginationParam )
     }
+    /**
+     * Converts give date into localized formatted date. If user want to have default Date to current date if date
+     * not provide, need to pass needDefaultDate true
+     *
+     * @param strDate
+     * @return
+     */
+    def convertToLocaleBasedDate( strDate ) {
+        try {
+            return LocalizeUtil.parseDate( dateConverterService.parseDefaultCalendarToGregorian( strDate ) )
+        }
+        catch (ApplicationException e) {
+            LOGGER.error( 'Error while parsing date' + e )
+            throw new ApplicationException( ActionItemProcessingCommonService, new BusinessLogicValidationException( 'invalid.date.format', [] ) )
+        }
+    }
+
 }
