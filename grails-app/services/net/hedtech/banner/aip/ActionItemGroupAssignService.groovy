68b95c68f7e774dbf4ab03668b7fa591c80ef50a
Jin Woo Shin <cootang2@gmail.com>
2017-10-30 21:06:16 -0600

    refactor assign actionitem to group

--- grails-app/services/net/hedtech/banner/aip/ActionItemGroupAssignService.groovy
+++ grails-app/services/net/hedtech/banner/aip/ActionItemGroupAssignService.groovy
@@ -10,68 +10,14 @@ import org.springframework.transaction.annotation.Propagation
 
 class ActionItemGroupAssignService  extends ServiceBase  {
 
-    def actionItemGroupAssignReadOnlyService
-
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = ApplicationException.class)
-    def updateActionItemGroupAssignment(aipUser, inputGroupAssignments, Long groupId ) {
-        def groupAssignment = ActionItemGroupAssign.fetchByGroupId(groupId)
-        List<Long>inputAssignActionItemIds = inputGroupAssignments.actionItemId.toList().collect{new Long(it)}
-        List<Long>existingAssignId = groupAssignment.actionItemId.toList()
-        def deleteActionItems = existingAssignId - inputAssignActionItemIds
-
-        //prepare create/update items
-        List<ActionItemGroupAssign> assignList = []
-        inputGroupAssignments.each { assignment ->
-            def assign = ActionItemGroupAssign.fetchByActionItemIdAndGroupId(assignment.actionItemId ,groupId)
-            if( assign.hasProperty("id")) {
-                //update
-                assign.groupId = groupId
-                assign.actionItemId = assignment.actionItemId
-                assign.seqNo = assignment.seq
-                assign.lastModified = new Date()
-                assign.lastModifiedBy = aipUser.username
-            } else {
-                //create
-                assign = new ActionItemGroupAssign(
-                        groupId: groupId,
-                        actionItemId: assignment.actionItemId,
-                        seqNo: assignment.seq
-                )
-            }
-            assignList.push(assign)
-        }
-        List<Long> deleteList = []
-        deleteActionItems.each { actionItemId ->
-            def assign = ActionItemGroupAssign.fetchByActionItemIdAndGroupId(actionItemId, groupId)
-            if (assign.hasProperty("id")) {
-                deleteList.push(assign.id)
-            } else {
-                throw ApplicationException("Deleting item does not exist")
-            }
-        }
-
-        def weGood = true
-        List<ActionItemGroupAssignReadOnly> updatedActionItemGroupAssignment
-        try {
-            //do preupdate to test
-            assignList.each {item -> this.preUpdate(item)}
-        } catch (net.hedtech.banner.exceptions.ApplicationException ae) {
-            weGood = false
-            throw new ApplicationException("rollback", ae.message, ae.defaultMessage)
-        }
-        if (weGood) {
-            try {
-                //delete
-                this.delete(deleteList)
-                //createOrUpdate
-                this.createOrUpdate(assignList, false)
-                updatedActionItemGroupAssignment = actionItemGroupAssignReadOnlyService.getAssignedActionItemsInGroup(groupId)
-            } catch (ApplicationException ae) {
-                throw new ApplicationException("rollback", ae.message, ae.defaultMessage)
-            }
-        }
-        return updatedActionItemGroupAssignment
+    def fetchByGroupId(Long id) {
+        ActionItemGroupAssign.fetchByGroupId( id )
     }
+
+    def fetchByActionItemIdAndGroupId (Long actionItemId, Long groupId) {
+        ActionItemGroupAssign.fetchByActionItemIdAndGroupId(actionItemId, groupId)
+    }
+
 
     def preCreate(domainModelOrMap) {
 
