c1a61f27e95a9160081d3fdd412f56f4683e99f9
Tiwari Shiv <shiv.tiwari@ellucian.com>
2017-11-02 20:04:39 +0530

    Code improvement

--- grails-app/services/net/hedtech/banner/aip/post/grouppost/ActionItemPostDetailService.groovy
+++ grails-app/services/net/hedtech/banner/aip/post/grouppost/ActionItemPostDetailService.groovy
@@ -10,13 +10,4 @@ import org.springframework.security.core.context.SecurityContextHolder
  * Service class for Action Item Posting Details
  */
 class ActionItemPostDetailService extends ServiceBase {
-    def preCreate( domainModelOrMap ) {
-            ActionItemPostDetail groupSend = (domainModelOrMap instanceof Map ? domainModelOrMap?.domainModel : domainModelOrMap) as ActionItemPostDetail
-            if (groupSend.getLastModifiedBy(  ) == null) {
-                groupSend.setLastModifiedBy( SecurityContextHolder?.context?.authentication?.principal?.getOracleUserName() )
-            }
-            if (groupSend.getLastModified() == null) {
-                groupSend.setLastModified( new Date() )
-            }
-        }
 }
