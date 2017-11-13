2a7ca5370098345c6746cb7a28d1307f3a6ec507
Craig Riddle <craig.riddle@ellucian.com>
2017-11-13 21:52:34 +0000

    devOps cleanup

--- test/integration/net/hedtech/banner/aip/post/grouppost/ActionItemPostReadOnlyIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/aip/post/grouppost/ActionItemPostReadOnlyIntegrationTests.groovy
@@ -4,6 +4,7 @@
 
 package net.hedtech.banner.aip.post.grouppost
 
+import net.hedtech.banner.aip.ActionItemGroup
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
@@ -25,9 +26,54 @@ class ActionItemPostReadOnlyIntegrationTests extends BaseIntegrationTestCase {
 
 
     @Test
-    void testFetchJobs() {
+    void testAllFetchJobs() {
+        def ourName = 'kdsfkgeddd'
+
+        ActionItemPost myAip = newActionItemPost( ourName )
+        myAip.save()
+
+        assert ActionItemPost.findAllByPostingName( ourName ).size() > 0
+        assert ActionItemPostReadOnly.findAllByPostingName( ourName ).size() > 0
+
         List<ActionItemPostReadOnly> actionItemReadPostOnlyList = ActionItemPostReadOnly.fetchJobs( [searchParam: '%'], [max: 1000, offset: 0] )
         assert actionItemReadPostOnlyList.size() > 0
     }
+
+
+    @Test
+    void testFetchJobByName() {
+        def ourName = 'asdfkdsfsskgedddfgr'
+
+        ActionItemPost myAip = newActionItemPost( ourName )
+        myAip.save()
+
+        assert ActionItemPost.findAllByPostingName( ourName ).size() > 0
+        assert ActionItemPostReadOnly.findAllByPostingName( ourName ).size() > 0
+
+        List<ActionItemPostReadOnly> actionItemReadPostOnlyList = ActionItemPostReadOnly.fetchJobs(
+                [searchParam: '%' + ourName + '%'], [max: 1000, offset: 0] )
+
+        assert actionItemReadPostOnlyList.size() > 0
+        assertEquals( ourName, actionItemReadPostOnlyList[0].postingName )
+    }
+
+
+    private ActionItemPost newActionItemPost( String name ) {
+            ActionItemPost aip = new ActionItemPost()
+            aip.populationVersionId = 32548072L
+            aip.populationListId = 43987234L
+            aip.postingName = name
+            aip.postingActionItemGroupId = ActionItemGroup.fetchActionItemGroups()[0]?.id
+            aip.postingDeleteIndicator = 'N'
+            aip.postingDisplayStartDate = new Date()
+            aip.postingDisplayEndDate = new Date() + 1
+            aip.postingCreatorId = 'rgre'
+            aip.postingCreationDateTime = new Date()
+            aip.populationRegenerateIndicator = 'N'
+            aip.lastModified = new Date()
+            aip.lastModifiedBy = 'edffeds'
+
+            return aip
+        }
 
 }
