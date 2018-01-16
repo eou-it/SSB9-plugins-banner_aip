51c02eed5e0577017c2128fd71ccf68541f340e3
Shiv Tiwari <shiv.tiwari@ellucian.com>
2018-01-16 21:33:02 +0530

    More coverage

--- test/integration/net/hedtech/banner/aip/post/grouppost/ActionItemPostWorkProcessorServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/aip/post/grouppost/ActionItemPostWorkProcessorServiceIntegrationTests.groovy
@@ -1,27 +1,38 @@
 package net.hedtech.banner.aip.post.grouppost
 
-import net.hedtech.banner.aip.post.ActionItemErrorCode
+import net.hedtech.banner.aip.ActionItemGroup
+import net.hedtech.banner.aip.ActionItemGroupAssign
+import net.hedtech.banner.general.communication.population.CommunicationPopulation
+import net.hedtech.banner.general.communication.population.CommunicationPopulationListView
+import net.hedtech.banner.general.communication.population.CommunicationPopulationVersion
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+import java.text.SimpleDateFormat
 
 class ActionItemPostWorkProcessorServiceIntegrationTests extends BaseIntegrationTestCase {
 
     def actionItemPostWorkProcessorService
+    def actionItemPostCompositeService
+    def actionItemProcessingCommonService
+    def springSecurityService
+    def actionItemPostService
 
 
     @Before
-    public void setUp() {
+    void setUp() {
         formContext = ['GUAGMNU']
         super.setUp()
+        loginSSB( 'AIPADM001', '111111' )
     }
 
 
     @After
-    public void tearDown() {
+    void tearDown() {
         super.tearDown()
+        logout()
     }
 
     // TODO: try posting to bad pidm
@@ -34,40 +45,51 @@ class ActionItemPostWorkProcessorServiceIntegrationTests extends BaseIntegration
     @Test
     // FIXME: not working
     void testPerformGroupSendItem() {
+        def user = springSecurityService.getAuthentication()?.user
         ActionItemPostWork actionItemPostWork = new ActionItemPostWork()
         actionItemPostWork.referenceId = 'somestringfortestinglswefhihvciewranc'
+        actionItemPostWork.recipientPidm = user.pidm
         actionItemPostWork.currentExecutionState = ActionItemPostWorkExecutionState.Ready
         actionItemPostWork.creationDateTime = new Date()
         ActionItemPost aip = newAIP()
+        aip = actionItemPostService.create( aip )
+        actionItemPostCompositeService.createPostItems( aip )
         aip.postingCurrentState = ActionItemPostExecutionState.New
-        aip.save()
         actionItemPostWork.actionItemGroupSend = aip
-        actionItemPostWork.save()
+        actionItemPostWork.save( flush: true )
         actionItemPostWorkProcessorService.performPostItem( actionItemPostWork )
     }
 
 
     private def newAIP() {
-        new ActionItemPost(
-                populationListId: 1L,
-                populationVersionId: 1L,
-                postingName: "some name",
-                postingDeleteIndicator: false,
-                postingScheduleType: "some type",
-                postingDisplayStartDate: new Date(),
-                postingDisplayEndDate: new Date(),
-                postingCreatorId: 'me',
-                postingScheduleDateTime: new Date(),
-                populationRegenerateIndicator: false,
-                postingCurrentState: ActionItemPostExecutionState.New,
-                postingStartedDate: null,
-                postingStopDate: null,
-                aSyncJobId: "la43j45h546k56g6f6r77a7kjfn",
-                populationCalculationId: 1L,
-                postingErrorCode: ActionItemErrorCode.DATA_FIELD_SQL_ERROR,
-                postingErrorText: null,
-                aSyncGroupId: null,
-                postingParameterValues: null
-        )
+        getInstance()
+    }
+
+
+    private getInstance() {
+        CommunicationPopulation population = CommunicationPopulation.findAllByPopulationName( 'AIP Student Population 1' )[0]
+        CommunicationPopulationVersion populationVersion = CommunicationPopulationVersion.findLatestByPopulationId( population.id )
+
+        SimpleDateFormat testingDateFormat = new SimpleDateFormat( 'MM/dd/yyyy' )
+        CommunicationPopulationListView populationListView = actionItemProcessingCommonService.fetchPopulationListForSend( 'p', [max: 10, offset:
+                0] )[0]
+        List<ActionItemGroup> actionItemGroups = ActionItemGroup.fetchActionItemGroups()
+        def actionItemGroup = actionItemGroups[0]
+        List<Long> actionItemIds = ActionItemGroupAssign.fetchByGroupId( actionItemGroup.id ).collect {it.actionItemId}
+        def requestMap = [:]
+        requestMap.postingName = 'testPostByPopulationSendInTwoMinutes'
+        requestMap.populationId = populationListView.id
+        requestMap.referenceId = UUID.randomUUID().toString()
+        requestMap.postingActionItemGroupId = actionItemGroup.id
+        requestMap.postNow = true
+        requestMap.recalculateOnPost = false
+        requestMap.displayStartDate = testingDateFormat.format( new Date() )
+        requestMap.displayEndDate = testingDateFormat.format( new Date() + 50 )
+        requestMap.scheduledStartDate = new Date() + 1
+        requestMap.actionItemIds = actionItemIds
+        def actionItemPost = actionItemPostCompositeService.getActionPostInstance( requestMap, springSecurityService.getAuthentication()?.user )
+        actionItemPost.populationCalculationId = populationVersion.id
+        actionItemPost.populationVersionId = populationVersion.id
+        actionItemPost
     }
 }
