fb12d43e6520147a1bcb7218c86f7d99f6f391dd
Craig Riddle <craig.riddle@ellucian.com>
2018-01-25 19:11:07 +0000

    fixing copyrights

--- BannerAipGrailsPlugin.groovy
+++ BannerAipGrailsPlugin.groovy
@@ -1,3 +1,6 @@
+/********************************************************************************
+  Copyright 2017 Ellucian Company L.P. and its affiliates.
+********************************************************************************/
 class BannerAipGrailsPlugin {
 
     String groupId = "net.hedtech"
