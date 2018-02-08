cb2b090a1ae91fb00b747cde87f11274f704a427
Supritha Kulkarni <supritha.shrikantkulkarni@ellucian.com>
2018-02-08 14:58:51 +0530

    CSAT-4248 - AIP: Action Item posting - query a scheduled job

--- grails-app/services/net/hedtech/banner/aip/post/grouppost/ActionItemPostReadOnlyService.groovy
+++ grails-app/services/net/hedtech/banner/aip/post/grouppost/ActionItemPostReadOnlyService.groovy
@@ -6,6 +6,8 @@ package net.hedtech.banner.aip.post.grouppost
 import net.hedtech.banner.aip.common.AIPConstants
 import net.hedtech.banner.i18n.MessageHelper
 import net.hedtech.banner.service.ServiceBase
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Service class for Action Item Posting Details Read Only
@@ -17,30 +19,30 @@ class ActionItemPostReadOnlyService extends ServiceBase {
      * @param params
      * @return
      */
-    def listActionItemPostJobList( params, paginationParams ) {
+    def listActionItemPostJobList(params, paginationParams) {
         params.searchParam = params.searchParam ? ('%' + params.searchParam.toUpperCase() + '%') : ('%')
-        def results = ActionItemPostReadOnly.fetchJobs( params, paginationParams )
-        results = results.collect {ActionItemPostReadOnly it ->
-            [
-                    id                     : it.postingId,
-                    postingId              : it.postingId,
-                    postingCurrentState    : it.postingCurrentState,
-                    jobState               : MessageHelper.message( 'aip.action.item.post.job.state.' + ActionItemPostExecutionState.getStateEnum( it.postingCurrentState ) ),
-                    postingName            : it.postingName,
+        def results = ActionItemPostReadOnly.fetchJobs(params, paginationParams)
+        results = results.collect {
+            ActionItemPostReadOnly it -> [
+                    id: it.postingId,
+                    postingId: it.postingId,
+                    postingCurrentState: it.postingCurrentState,
+                    jobState: MessageHelper.message('aip.action.item.post.job.state.' + ActionItemPostExecutionState.getStateEnum(it.postingCurrentState)),
+                    postingName: it.postingName,
                     postingDisplayStartDate: it.postingScheduleDateTime ? it.postingScheduleDateTime : it.postingDisplayStartDate,
-                    postingStartedDate     : it.postingStartedDate,
-                    groupFolderName        : it.groupFolderName,
-                    postingPopulation      : it.postingPopulation,
-                    groupName              : it.groupName,
-                    postingCreatorId       : it.postingCreatorId,
-                    lastModified           : it.lastModified,
-                    lastModifiedBy         : it.lastModifiedBy,
-                    version                : it.version
+                    postingStartedDate: it.postingStartedDate,
+                    groupFolderName: it.groupFolderName,
+                    postingPopulation: it.postingPopulation,
+                    groupName: it.groupName,
+                    postingCreatorId: it.postingCreatorId,
+                    lastModified: it.lastModified,
+                    lastModifiedBy: it.lastModifiedBy,
+                    version: it.version
 
 
             ]
         }
-        def resultCount = fetchJobsCount( params )
+        def resultCount = fetchJobsCount(params)
         def resultMap = [
                 result: results,
                 length: resultCount,
@@ -53,23 +55,62 @@ class ActionItemPostReadOnlyService extends ServiceBase {
      *
      * @return
      */
-    def fetchJobsCount( params ) {
+    def fetchJobsCount(params) {
         params.searchParam = params.searchParam ? ('%' + params.searchParam.toUpperCase() + '%') : ('%')
-        ActionItemPostReadOnly.fetchJobsCount( params )
+        ActionItemPostReadOnly.fetchJobsCount(params)
     }
-	
-	/**
-     * Lists Post status
-     *
-     * @return
-     */
-	def statusPosted(postingId) {
-		ActionItemPostReadOnly actionItemPostReadOnly = ActionItemPostReadOnly.fetchByPostingId(postingId)
-		if (actionItemPostReadOnly) {
-			if (actionItemPostReadOnly.postingCurrentState.equals( "Scheduled" ) ){
-				return AIPConstants.YES_IND
-			}
-		}
-		return AIPConstants.NO_IND
-	}
+
+    def statusPosted(postingId) {
+        ActionItemPostReadOnly actionItemPostReadOnly = ActionItemPostReadOnly.fetchByPostingId(postingId)
+        if (actionItemPostReadOnly) {
+            if (actionItemPostReadOnly.postingCurrentState.equals( "Scheduled") ){
+                return AIPConstants.YES_IND
+            }
+        }
+        return AIPConstants.NO_IND
+    }
+    def  JobDetailsByPostId(postingId) {
+        ActionItemPostReadOnly actionItemPostReadOnly = ActionItemPostReadOnly.fetchByPostingId(postingId)
+        def result ={}
+        SimpleDateFormat timeFormat = new SimpleDateFormat(MessageHelper.message("default.time.format"));//Initializing the date format
+        def timeZone = TimeZone.getDefault();//Getting the time zone of the server
+
+        if (actionItemPostReadOnly)
+        {
+            result = [
+                groupFolderId:actionItemPostReadOnly.groupFolderId,
+                groupFolderName:actionItemPostReadOnly.groupFolderName,
+                groupName:actionItemPostReadOnly.groupName,
+                groupStatus:actionItemPostReadOnly.groupStatus,
+                groupTitle:actionItemPostReadOnly.groupTitle,
+                lastModified:actionItemPostReadOnly.lastModified,
+                lastModifiedBy:actionItemPostReadOnly.lastModifiedBy,
+                populationCalculationId:actionItemPostReadOnly.populationCalculationId,
+                populationListId:actionItemPostReadOnly.populationListId,
+                populationRegenerateIndicator:actionItemPostReadOnly.populationRegenerateIndicator,
+                postingActionItemGroupId:actionItemPostReadOnly.postingActionItemGroupId,
+                postingCreationDateTime:actionItemPostReadOnly.postingCreationDateTime,
+                postingCreatorId:actionItemPostReadOnly.postingCreatorId,
+                postingCurrentState:actionItemPostReadOnly.postingCurrentState,
+                postingDeleteIndicator:actionItemPostReadOnly.postingDeleteIndicator,
+                postingDisplayEndDate:actionItemPostReadOnly.postingDisplayEndDate,
+                postingDisplayStartDate:actionItemPostReadOnly.postingDisplayStartDate,
+                postingErrorCode:actionItemPostReadOnly.postingErrorCode,
+                postingErrorText:actionItemPostReadOnly.postingErrorText,
+                postingId:actionItemPostReadOnly.postingId,
+                postingName:actionItemPostReadOnly.postingName,
+                postingParameterValues:actionItemPostReadOnly.postingParameterValues,
+                postingPopulation:actionItemPostReadOnly.postingPopulation,
+                postingScheduleDateTime:actionItemPostReadOnly.postingScheduleDateTime,
+                postingStartedDate:actionItemPostReadOnly.postingStartedDate,
+                version:actionItemPostReadOnly.version,
+                scheduledStartTime : actionItemPostReadOnly.postingScheduleDateTime ? timeFormat.format(actionItemPostReadOnly.postingScheduleDateTime) : actionItemPostReadOnly.postingScheduleDateTime,
+                timezoneStringOffset  : timeZone
+
+            ]
+
+        }
+        return result
+    }
+
 }
