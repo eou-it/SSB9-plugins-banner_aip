19689296fb6573a25f396a2bbad63a7acb212326
Shiv Tiwari <shiv.tiwari@ellucian.com>
2018-02-21 12:16:16 +0530

    Fixed test cases

--- test/integration/net/hedtech/banner/aip/ActionItemReadOnlyServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/aip/ActionItemReadOnlyServiceIntegrationTests.groovy
@@ -4,6 +4,7 @@
 
 package net.hedtech.banner.aip
 
+import net.hedtech.banner.general.communication.folder.CommunicationFolder
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
@@ -59,10 +60,18 @@ class ActionItemReadOnlyServiceIntegrationTests extends BaseIntegrationTestCase 
 
     @Test
     void testActionItemSortNameAsc() {
+        def folderId = CommunicationFolder.findByName( 'AIPstudent' ).id
+        new ActionItem( name: 'A-INT-TEST1', title: 'A-INT-TEST1', folderId: folderId, status: 'D', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'B-INT-TEST1', title: 'B-INT-TEST1', folderId: folderId, status: 'I', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'C-INT-TEST1', title: 'C-INT-TEST1', folderId: folderId, status: 'A', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'D-INT-TEST1', title: 'D-INT-TEST1', folderId: folderId, status: 'D', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'E-INT-TEST1', title: 'E-INT-TEST1', folderId: folderId, status: 'I', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'F-INT-TEST1', title: 'F-INT-TEST1', folderId: folderId, status: 'A', postedIndicator: 'N' ).save( flush: true )
+        new ActionItem( name: 'G-INT-TEST1', title: 'G-INT-TEST1', folderId: folderId, status: 'I', postedIndicator: 'N' ).save( flush: true )
         def results = actionItemReadOnlyService.fetchWithPagingAndSortParams(
-                [filterName: "%", sortColumn: "actionItemName", sortAscending: true, max: 20, offset: 0] )
+                [filterName: "%-INT-TEST1", sortColumn: "actionItemName", sortAscending: true, max: 7, offset: 0] )
 
-        assertEquals( 20, results.size() )
+        assertEquals( 7, results.size() )
         def asFound = []
         results.each {it ->
             asFound.add( it.actionItemName )
