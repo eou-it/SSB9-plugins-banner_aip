2d93c085368ca60850f9dc915dc9d2c5aba0496c
sivaramn <sivaram.nyapathi@ellucian.com>
2019-01-11 14:28:15 +0530

    NO JIRA : Code clean up - Unused method has been removed

--- grails-app/services/net/hedtech/banner/aip/UploadDocumentContentService.groovy
+++ grails-app/services/net/hedtech/banner/aip/UploadDocumentContentService.groovy
@@ -1,5 +1,5 @@
 /*********************************************************************************
- Copyright 2018 Ellucian Company L.P. and its affiliates.
+ Copyright 2018-2019 Ellucian Company L.P. and its affiliates.
  ********************************************************************************* */
 
 package net.hedtech.banner.aip
@@ -11,13 +11,6 @@ import net.hedtech.banner.service.ServiceBase
  */
 class UploadDocumentContentService extends ServiceBase {
 
-    /**
-     * Validation before uploading document
-     * @param dataMap
-     */
-    def preCreateValidation( domainModelOrMap ) {
-        UploadDocumentContentService udc = (domainModelOrMap instanceof Map ? domainModelOrMap?.domainModel : domainModelOrMap) as UploadDocumentContentService
-        }
     /**
      * This method is responsible for getting content of attached document based on ID.
      * @param params
