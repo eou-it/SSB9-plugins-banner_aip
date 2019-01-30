d60bc2ff3d91181479abb368d19ce8914a422cd3
Manjunatha Gopalapura <manjunatha.dg@ellucian.com>
2019-01-30 19:57:49 +0530

    Comment added.

--- grails-app/services/net/hedtech/banner/aip/AipReviewStateService.groovy
+++ grails-app/services/net/hedtech/banner/aip/AipReviewStateService.groovy
@@ -15,6 +15,13 @@ class AipReviewStateService extends ServiceBase {
 
     /**
      * Returns Review State record for the combination of code and locale.
+     * Here is how it is evaluated,
+     * 1. check if chosen locale is a "two-parter" or "sub-locale" (e.g. fr_CA), then take the initial part of locale as primary locale
+     * 2. get Review State records for a given locale, primary locale, default locale (en_US)
+     * 3. check if Review State record available for chosen locale, if so return the record
+     * 4. if not found for chosen locale, then check for primary locale
+     * 5. if not found primary locale as well, then check for default locale
+     * 6. finaly if not found for any locale, then return null
      * @params code Code of Review State
      * @params locale User locale
      * @returns String Review State Name
@@ -47,6 +54,13 @@ class AipReviewStateService extends ServiceBase {
 
     /**
      * Returns list of Review States without the default code.     *
+     * Here is how it is evaluated,
+     * 1. check if chosen locale is a "two-parter" or "sub-locale" (e.g. fr_CA), then take the initial part of locale as primary locale
+     * 2. get Review State records for a given locale, primary locale, default locale (en_US)
+     * 3. check if Review State records available for chosen locale, if so return the record list
+     * 4. if not found for chosen locale, then check for primary locale and return the record list
+     * 5. if not found primary locale as well, then check for default locale and return the record list
+     * 6. finaly if not found for any locale, then return empty list
      * @params locale User locale
      * @returns List of Review States
      */
