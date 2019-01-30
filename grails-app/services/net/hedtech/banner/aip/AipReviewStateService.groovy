4c9c52f9f58f6b3a9efc6d6a7158bf9391f29d45
Manjunatha Gopalapura <manjunatha.dg@ellucian.com>
2019-01-30 19:18:13 +0530

    BS-10694, changes made to handle sublocale.

--- grails-app/services/net/hedtech/banner/aip/AipReviewStateService.groovy
+++ grails-app/services/net/hedtech/banner/aip/AipReviewStateService.groovy
@@ -8,7 +8,7 @@ import net.hedtech.banner.aip.common.AIPConstants
 import net.hedtech.banner.i18n.MessageHelper
 import net.hedtech.banner.service.ServiceBase
 
-/**
+/****
  * AIP Review State Services.
  */
 class AipReviewStateService extends ServiceBase {
@@ -20,11 +20,20 @@ class AipReviewStateService extends ServiceBase {
      * @returns String Review State Name
      */
      String fetchReviewStateNameByCodeAndLocale(String code, String locale) {
-        String reviewStateName = null
-        List<AipReviewState> reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale(code, locale)
+        def primaryLocale
+        if(locale.contains("_")) {
+            primaryLocale = locale.substring(0, locale.indexOf("_"))
+        }
 
+        List<AipReviewState> reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale(code, locale, primaryLocale)
         AipReviewState aipReviewState = reviewStateResult.find {it->
             it.locale.toUpperCase() == locale.toUpperCase()
+        }
+
+        if(!aipReviewState) {
+            aipReviewState = reviewStateResult.find { it ->
+                it.locale.toUpperCase() == primaryLocale.toUpperCase()
+            }
         }
 
         if(!aipReviewState) {
@@ -33,8 +42,8 @@ class AipReviewStateService extends ServiceBase {
             }
         }
 
-        reviewStateName = aipReviewState?.reviewStateName
-    }
+        aipReviewState?.reviewStateName
+     }
 
     /**
      * Returns list of Review States without the default code.     *
@@ -42,19 +51,40 @@ class AipReviewStateService extends ServiceBase {
      * @returns List of Review States
      */
     List<AipReviewState> fetchNonDefaultReviewStates(String locale) {
-        List<AipReviewState> reviewStateResult = AipReviewState.fetchNonDefaultReviewStates(locale)
+        def primaryLocale
+        if(locale.contains("_")) {
+            primaryLocale = locale.substring(0, locale.indexOf("_"))
+        }
+
+        List<AipReviewState> reviewStateResult = AipReviewState.fetchNonDefaultReviewStates(locale, primaryLocale)
 
         List<AipReviewState> localeReviewStateResult = new ArrayList<AipReviewState>()
+        List<AipReviewState> primaryLocaleReviewStateResult = new ArrayList<AipReviewState>()
         List<AipReviewState> defaultLocaleReviewStateResult = new ArrayList<AipReviewState>()
 
         reviewStateResult.each() { it ->
             if(it.locale.toUpperCase() == locale.toUpperCase()) {
                 localeReviewStateResult.add(it)
-            } else if (it.locale.toUpperCase() == AIPConstants.DEFAULT_LOCALE) {
-                defaultLocaleReviewStateResult.add(it)
             }
         }
 
-        localeReviewStateResult ? localeReviewStateResult : (defaultLocaleReviewStateResult ? defaultLocaleReviewStateResult : [])
+        if(!localeReviewStateResult && primaryLocale) {
+            reviewStateResult.each() { it ->
+                if (it.locale.toUpperCase() == primaryLocale.toUpperCase()) {
+                    primaryLocaleReviewStateResult.add(it)
+                }
+            }
+        }
+
+        if(!localeReviewStateResult && !primaryLocaleReviewStateResult) {
+            reviewStateResult.each() { it ->
+                if (it.locale.toUpperCase() == AIPConstants.DEFAULT_LOCALE) {
+                    defaultLocaleReviewStateResult.add(it)
+                }
+            }
+        }
+
+        localeReviewStateResult ? localeReviewStateResult : (primaryLocaleReviewStateResult ? primaryLocaleReviewStateResult :
+                (defaultLocaleReviewStateResult ? defaultLocaleReviewStateResult : []))
     }
 }
--- test/integration/net/hedtech/banner/aip/AipReviewStateServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/aip/AipReviewStateServiceIntegrationTests.groovy
@@ -36,29 +36,56 @@ class AipReviewStateServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     void testFetchReviewStateNameByCodeAndLocale() {
-        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10','EN_US')
+        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'EN_US')
         assert "Review needed", reviewStateName
 
-        reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10','es')
+        reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'es')
         assert "Revisi��n necesaria", reviewStateName
     }
 
     @Test
     void testFetchReviewStateNameOfDefaultLocale() {
         //when for the specified locale ReviewState is not available, ReviewState of default locale (en_US) is returned
-        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10','fr_CA')
+        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'fr_CA')
         assert "Review needed", reviewStateName
     }
 
     @Test
     void testReviewStateDoesNotExist() {
-        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('11','EN_US')
-        assertNull( reviewStateName )
+        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('11', 'EN_US')
+        assertNull(reviewStateName)
 
         AipReviewState reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale("10", "en_US")[0]
         reviewStateResult.locale = 'AB'
         reviewStateResult.save(flush: true, failOnError: true)
-        reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10','gb')
+        reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'gb')
+        assertNull reviewStateName
+    }
+
+    @Test
+    void testFetchReviewStateNameForFrenchFranceLocale() {
+        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'fr_FR')
+        assert "Révision requise", reviewStateName
+
+        AipReviewState reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale("10", "fr")[0]
+        reviewStateResult.locale = 'AB'
+        reviewStateResult.save(flush: true, failOnError: true)
+
+        reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'fr_FR')
+        assert "Review needed", reviewStateName
+    }
+
+    @Test
+    void testFetchReviewStateNameNull() {
+        AipReviewState reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale("10", "en_US")[0]
+        reviewStateResult.locale = 'CD'
+        reviewStateResult.save(flush: true, failOnError: true)
+
+        reviewStateResult = AipReviewState.fetchReviewStateByCodeAndLocale("10", "fr")[0]
+        reviewStateResult.locale = 'AB'
+        reviewStateResult.save(flush: true, failOnError: true)
+
+        String reviewStateName = aipReviewStateService.fetchReviewStateNameByCodeAndLocale('10', 'fr_FR')
         assertNull reviewStateName
     }
 
@@ -66,27 +93,27 @@ class AipReviewStateServiceIntegrationTests extends BaseIntegrationTestCase {
     void testFetchNonDefaultReviewStates() {
         List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("pt")
         assert reviewStateResult.size() == 6
-        def reviewStateCodes = reviewStateResult.collect{it->
-            if(it.locale.toUpperCase() == "PT") {
+        def reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == "PT") {
                 it.reviewStateCode
             }
         }
-        reviewStateCodes = reviewStateResult.collect{it.reviewStateCode }
+        reviewStateCodes = reviewStateResult.collect { it.reviewStateCode }
         assert reviewStateCodes.containsAll(['20', '30', '40', '50', '60', '70'])
-        assertFalse (reviewStateCodes.contains('10'))
+        assertFalse(reviewStateCodes.contains('10'))
     }
 
     @Test
     void testFetchNonDefaultReviewStatesLocaleNotExists() {
         List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("ABC")
         assert reviewStateResult.size() == 6
-        def reviewStateCodes = reviewStateResult.collect{it->
-            if(it.locale.toUpperCase() == AIPConstants.DEFAULT_LOCALE) {
+        def reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == AIPConstants.DEFAULT_LOCALE) {
                 it.reviewStateCode
             }
         }
         assert reviewStateCodes.containsAll(['20', '30', '40', '50', '60', '70'])
-        assertFalse (reviewStateCodes.contains('10'))
+        assertFalse(reviewStateCodes.contains('10'))
     }
 
     @Test
@@ -94,7 +121,7 @@ class AipReviewStateServiceIntegrationTests extends BaseIntegrationTestCase {
         List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("en_US")
 
         reviewStateResult.each() { it ->
-            if(it.locale.toUpperCase() == "EN_US") {
+            if (it.locale.toUpperCase() == "EN_US") {
                 it.locale = 'ABC'
                 it.save(flush: true, failOnError: true)
             }
@@ -103,4 +130,90 @@ class AipReviewStateServiceIntegrationTests extends BaseIntegrationTestCase {
         reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("en_US")
         assert reviewStateResult.isEmpty()
     }
+
+    @Test
+    void testFetchNonDefaultReviewStatesFrenchFrance() {
+
+        List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("fr_FR")
+        assert reviewStateResult.size() > 0
+        def fr_FR_reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == "FR_FR") {
+                it.reviewStateCode
+            }
+        }
+        assert fr_FR_reviewStateCodes.containsAll([null, null, null, null, null, null])
+
+        def fr_reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == "FR") {
+                it.reviewStateCode
+            }
+        }
+
+        assert fr_reviewStateCodes.containsAll(['20', '30', '40', '50', '70'])
+        assertFalse(fr_reviewStateCodes.contains('10'))
+    }
+
+    @Test
+    void testFetchNonDefaultReviewStatesFrenchFranceDefaultToDefaultLocale() {
+        List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("fr")
+
+        reviewStateResult.each() { it ->
+            if (it.locale.toUpperCase() == "FR") {
+                it.locale = 'ABC'
+                it.save(flush: true, failOnError: true)
+            }
+        }
+
+        reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("fr_FR")
+        assert reviewStateResult.size() > 0
+
+        def fr_FR_reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == "FR_FR") {
+                it.reviewStateCode
+            }
+        }
+        assert fr_FR_reviewStateCodes.containsAll([null, null, null, null, null, null])
+
+        def fr_reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == "FR") {
+                it.reviewStateCode
+            }
+        }
+        assert fr_reviewStateCodes.containsAll([null, null, null, null, null, null])
+
+
+        def default_reviewStateCodes = reviewStateResult.collect { it ->
+            if (it.locale.toUpperCase() == AIPConstants.DEFAULT_LOCALE) {
+                it.reviewStateCode
+            }
+        }
+        assert default_reviewStateCodes.size() > 0
+        assert default_reviewStateCodes.containsAll(['20', '30', '40', '50', '60', '70'])
+        assertFalse(default_reviewStateCodes.contains('10'))
+    }
+
+    @Test
+    void testFetchNonDefaultReviewStatesForFrenchFranceDefaultLocaleNotExists() {
+        List<AipReviewState> reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("en_US")
+
+        reviewStateResult.each() { it ->
+            if (it.locale.toUpperCase() == "EN_US") {
+                it.locale = 'ABC'
+                it.save(flush: true, failOnError: true)
+            }
+        }
+
+        reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("fr")
+
+        reviewStateResult.each() { it ->
+            if (it.locale.toUpperCase() == "FR") {
+                it.locale = 'XYZ'
+                it.save(flush: true, failOnError: true)
+            }
+        }
+
+        reviewStateResult = aipReviewStateService.fetchNonDefaultReviewStates("fr_FR")
+        assert reviewStateResult.isEmpty()
+    }
+
 }
